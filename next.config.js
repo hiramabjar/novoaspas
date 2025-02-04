598d7d85754abfab23bba996954b6a053d36e1b8
Hiram Abjar <hiramabjar@github.com>
2025-02-04 16:28:26 -0300

    fix: configura rotas dinâmicas individualmente

--- next.config.js
+++ next.config.js
@@ -3,16 +3,7 @@ const nextConfig = {
   output: 'standalone',
   images: {
     domains: ['github.com'],
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb'
-    }
-  },
-  // Disable static generation
-  staticGeneration: false,
-  // Force dynamic rendering
-  dynamicRendering: true
+  }
 }
 
 module.exports = nextConfig 
