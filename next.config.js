f52d44ab7d12dd1dbcc93e01d8a8244cb342042f
Hiram Abjar <hiramabjar@github.com>
2025-02-04 16:06:06 -0300

    fix: remove configurações incompatíveis e ajusta setup do banco

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ['github.com'],
   },
-  // Disable static exports for API routes
-  exportPathMap: async function() {
-    return {
-      '/': { page: '/' },
-    }
-  }
 }
 
 module.exports = nextConfig 
